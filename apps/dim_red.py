ae27df90815259e0a4865397b41355506c975a0a
Edgar Manrique <edgar.manrique30@gmail.com>
2022-08-08 08:38:09 -0500

    dimred:mnfDescription

--- apps/dim_red.py
+++ apps/dim_red.py
@@ -7,6 +7,15 @@ def app():
 
     st.title("Dimensionality reduction")
 
+    st.markdown(
+    """
+    We used Minimum Noise Fraction (MNF) to reduce dimensionality of our data, 
+    briefly, MNF transform the bands and orders them based on their signal to noise ratio, 
+    which means that th MNF output contains steadily increasing image quality, as oposed to 
+    a PCA which is based on variance.
+    """
+    )
+
     image = Image.open('img/mnf_components.png')
 
     st.image(image, caption='MNF components')
